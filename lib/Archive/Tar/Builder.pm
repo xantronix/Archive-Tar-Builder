34d3751d9881cfba33e43661a26dfdd59b46d389
Erin Schoenhals <erin@cpanel.net>
2012-07-13 08:49:30 -0500

    Increment Archive::Tar::Builder version
    Case 60282: Increment Archive::Tar::Builder version to allow new CPAN release

--- lib/Archive/Tar/Builder.pm
+++ lib/Archive/Tar/Builder.pm
@@ -19,7 +19,7 @@ BEGIN {
     use vars qw(@ISA $VERSION);
 
     our @ISA     = qw(Exporter);
-    our $VERSION = '0.5_2012070501';
+    our $VERSION = '0.5_2012071301';
 }
 
 XSLoader::load( 'Archive::Tar::Builder', $VERSION );
